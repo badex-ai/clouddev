852fe04bb5b04d0ba8ed22941c133a76dc2d2ab6
Badamasi Aliu <Badamasi.link@gmail.com>
2025-07-10 01:39:33 +0100

    feat (backend): i created the mvc format for the project and also the requirement.txt fiel

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,14 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import HTTPBearer
+from fastapi.middleware.cors import CORSMiddleware
+from routes.user_route import router as user_router
+from routes.task_routes import router as task_router
+from routes.auth_routes import router as auth_router
+
+
+app = FastAPI()
+
+
+
+
+app.include_router(user_router, prefix="/users", tags=["users"])
