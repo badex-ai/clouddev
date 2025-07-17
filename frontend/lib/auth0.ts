4cf6686e22c5b6455c64fb73dee936f3bdb5df01
Badamasi Aliu <badamasi.link@gmail.com>
2025-07-17 02:14:27 +0100

    fix (frontend): made changes to the auth file

--- frontend/lib/auth0.ts
+++ frontend/lib/auth0.ts
@@ -1,10 +1,11 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
+
 export const auth0 = new Auth0Client({
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  appBaseUrl: process.env.AUTH0_BASE_URL,
+  appBaseUrl: process.env.APP_BASE_URL,
   secret: process.env.AUTH0_SECRET,
   authorizationParameters: {
     scope: process.env.AUTH0_SCOPE,
@@ -12,6 +13,3 @@ export const auth0 = new Auth0Client({
   }
 });
 
-// import { Auth0Client } from "@auth0/nextjs-auth0/server";
-
-// export const auth0 = new Auth0Client();
